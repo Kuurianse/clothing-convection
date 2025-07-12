9ba2583e1079b8b43b503040de9a7ba2c4880879
Surya Saddam Saputra <119725916+Kuurianse@users.noreply.github.com>
2025-07-12 14:30:57 +0700

    build(config): set base path for GitHub Pages deployment

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,8 @@ import { defineConfig } from 'vite'
 import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
+  // Tambahkan atau ubah baris 'base' ini
+  base: '/clothing-convection/', // <-- Sesuaikan dengan nama repo kamu!
   plugins: [
     tailwindcss(),
   ],
