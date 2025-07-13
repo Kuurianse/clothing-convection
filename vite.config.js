67e1ba55dbdd1ed8e227967547953b7c660f7590
Surya Saddam Saputra <119725916+Kuurianse@users.noreply.github.com>
2025-07-13 09:22:14 +0700

    change base vite

--- vite.config.js
+++ vite.config.js
@@ -3,8 +3,9 @@ import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
   // Tambahkan atau ubah baris 'base' ini
-  base: '/clothing-convection/dist/', // <-- Sesuaikan dengan nama repo!
+  // base: '/clothing-convection/dist/', // <-- Sesuaikan dengan nama repo!
   // base: 'https://github.com/Kuurianse/clothing-convection/dist/', // <-- Sesuaikan dengan nama repo!
+  base: 'https://kuurianse.github.io/clothing-convection/dist/', // <-- Sesuaikan dengan nama repo!
   plugins: [
     tailwindcss(),
   ],
